4dc5698932f67b50f41dc2691e48080247564ee3
juhaszadam <juhasz.adam@virgo.hu>
2015-06-23 13:37:20 +0200

    SimpleManager C1

--- test/Manager/SimpleManagerTest.php
+++ test/Manager/SimpleManagerTest.php
@@ -2,6 +2,7 @@
 
 namespace Shepard\Tests\Manager;
 
+use Shepard\Entity\SimpleJob;
 use Shepard\Manager\SimpleManager;
 
 class SimpleManagerTest extends \PHPUnit_Framework_TestCase
@@ -9,6 +10,28 @@ class SimpleManagerTest extends \PHPUnit_Framework_TestCase
     public function testSave()
     {
         $manager = new SimpleManager();
-        $this->assertTrue(true);
+        $job = $this->mockJob();
+        $deadline = new \DateTime("tomorrow");
+        $job->expects($this->once())
+            ->method('getAuthor')
+            ->will($this->returnValue("John Doe"));
+        $job->expects($this->once())
+            ->method('getDescription')
+            ->will($this->returnValue("Foo Bar."));
+        $job->expects($this->once())
+            ->method('getDeadline')
+            ->will($this->returnValue($deadline));
+
+        $manager->save($job);
+
+        $this->assertFileExists("/tmp/Shepard/Job.txt");
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function mockJob()
+    {
+        return $this->getMockBuilder(SimpleJob::class)->getMock();
     }
 }
