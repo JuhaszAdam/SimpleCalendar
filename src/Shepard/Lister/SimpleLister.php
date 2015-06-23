8ae1f144c453700d27d4cff142afab8e6d75ddd9
juhaszadam <juhasz.adam@virgo.hu>
2015-06-23 15:07:54 +0200

    SimpleLister C1

--- src/Shepard/Lister/SimpleLister.php
+++ src/Shepard/Lister/SimpleLister.php
@@ -1,14 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: juhaszadam
- * Date: 2015.06.23.
- * Time: 13:06
- */
 
 namespace Shepard\Lister;
 
+use Shepard\Entity\SimpleJob;
+use Shepard\Entity\SimpleUser;
+use Shepard\Manager\SimpleManager;
 
-class SimpleLister {
+class SimpleLister
+{
+    /**
+     * @var SimpleLister
+     */
+    private $manager;
 
+    public function __construct(SimpleManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function printJobs(SimpleUser $user)
+    {
+        $result = '<ul>';
+        foreach ($this->manager->loadJob() as $job) {
+            /** @var SimpleJob $job */
+            $deadline = $job->getDeadline();
+            $deadline->setTimezone($user->getTimezone());
+
+            $result .= sprintf("<li>%s - %s Due: %s</li>",
+                $job->getAuthor(),
+                $job->getDescription(),
+                $deadline->format("Y.m.d H:i:s")
+            );
+        }
+        $result .= '</ul>';
+
+        return $result;
+    }
 }
--- test/Lister/SimpleListerTest.php
+++ test/Lister/SimpleListerTest.php
@@ -1,6 +1,64 @@
 <?php
 
+use Shepard\Entity\SimpleJob;
+use Shepard\Entity\SimpleUser;
+use Shepard\Lister\SimpleLister;
+use Shepard\Manager\SimpleManager;
+
 class SimpleListerTest extends PHPUnit_Framework_TestCase
 {
+    public function testPrintJobs()
+    {
+        $lister = new SimpleLister($this->mockManager());
 
+        $this->assertStringMatchesFormat("%s - %s Due: %s", $lister->printJobs($this->mockUser()));
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function mockManager()
+    {
+        $manager = $this->getMockBuilder(SimpleManager::class)->getMock();
+        $job = $this->mockJob();
+        $manager->expects($this->once())
+            ->method('loadJob')
+            ->will($this->returnValue([$job]));
+
+        return $manager;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function mockJob()
+    {
+        $job = $this->getMockBuilder(SimpleJob::class)->getMock();
+        $deadline = new \DateTime("tomorrow");
+        $job->expects($this->once())
+            ->method('getAuthor')
+            ->will($this->returnValue("John Doe"));
+        $job->expects($this->once())
+            ->method('getDescription')
+            ->will($this->returnValue("Foo Bar."));
+        $job->expects($this->once())
+            ->method('getDeadline')
+            ->will($this->returnValue($deadline));
+
+        return $job;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function mockUser()
+    {
+        $job = $this->getMockBuilder(SimpleUser::class)->getMock();
+        $timezone = new \DateTimeZone("Europe/Budapest");
+        $job->expects($this->once())
+            ->method('getTimezone')
+            ->will($this->returnValue($timezone));
+
+        return $job;
+    }
 }
