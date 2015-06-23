e31e5d5bd4b19c14908e370ba521edb1eaefff05
juhaszadam <juhasz.adam@virgo.hu>
2015-06-23 13:19:07 +0200

    SimpleUser C1

--- test/Entity/SimpleUserTest.php
+++ test/Entity/SimpleUserTest.php
@@ -1,6 +1,14 @@
 <?php
 
+use Shepard\Entity\SimpleUser;
+
 class SimpleUserTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testGetterSetter()
+    {
+        $user = new SimpleUser();
+        $user->setName("Yamada Taro");
+        $user->setTimezone(new \DateTimeZone("Asia/Hong_Kong"));
+    }
 }
